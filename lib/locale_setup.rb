8143149f9ff13cd9b3e743bb6d2a08885ac97f7a
Andres Montano <amontano@virginia.edu>
2014-02-25 14:47:55 -0600

    dealing with apache returning ASCII-8BIT instead of UTF-8 in server

--- lib/locale_setup.rb
+++ lib/locale_setup.rb
@@ -4,7 +4,7 @@ module LocaleSetup
     available_locales = I18n.available_locales
     language = language.to_sym if !language.nil?
     if language.nil? || !available_locales.include?(language)
-      language = (request.env["HTTP_ACCEPT_LANGUAGE"] || "").scan(/[^,;]+/).collect {|l| l[0..1].to_sym}.find{|l| available_locales.include?(l)}
+      language = (request.env["HTTP_ACCEPT_LANGUAGE"].dup.force_encoding(Encoding::UTF_8) || "").scan(/[^,;]+/).collect {|l| l[0..1].to_sym}.find{|l| available_locales.include?(l)}
       language = I18n.default_locale if language.nil? || !available_locales.include?(language.to_sym) 
       session[:language] = language.to_s
     end
